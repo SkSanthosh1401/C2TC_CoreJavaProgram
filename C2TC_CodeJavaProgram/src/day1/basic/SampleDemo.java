e3e3292250e14204e1e0563cc93b3434d0e674ba
Lenovo <Lenovo@DESKTOP-6UOCQ3I>
2025-07-21 16:27:24 +0530

    ENTITY CLASS

--- C2TC_CodeJavaProgram/src/day1/basic/SampleDemo.java
+++ C2TC_CodeJavaProgram/src/day1/basic/SampleDemo.java
@@ -1,7 +1,11 @@
 package day1.basic;
 
 public class SampleDemo {
-
+    
+	public static void main()
+	{
+		System.out.println("Welcome form main 1")
+	}
 	public static void main(String[] args) {
 		System.out.println("Welcome to JFS...");
 		System.out.println("New line");
