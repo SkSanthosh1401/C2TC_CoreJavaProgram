53c2cec4940c365a6bbfff7fe184bcbb85b23809
Lenovo <Lenovo@DESKTOP-6UOCQ3I>
2025-07-21 22:34:15 +0530

    day2 loop statement and conditional statements

---
+++ C2TC_CodeJavaProgram/src/day1/entity/BreakDemo.java
@@ -0,0 +1,10 @@
+package day1.entity;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ C2TC_CodeJavaProgram/src/day1/entity/CustomerDemo.java
@@ -0,0 +1,29 @@
+package day1.entity;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer c1=new Customer();
+		c1.setCid(101);
+		c1.setCname("SANTHOSH");
+		c1.setCity("Puducherry");
+		System.out.println("The cid is "+c1.getCid());
+		System.out.println("The cname is "+c1.getCname());
+		System.out.println("The city name is "+c1.getCity());
+		System.out.println(c1);
+		//Customer 2
+		Customer c2=new Customer();
+		c2.setCid(69);
+		c2.setCname("ARAVINDH");
+		c2.setCity("Mumbai");
+		System.out.println("The cid is "+c2.getCid());
+		System.out.println("The cname is "+c2.getCname());
+		System.out.println("The city is "+c2.getCity());
+		System.out.println(c2);
+		
+		
+
+	}
+
+}
